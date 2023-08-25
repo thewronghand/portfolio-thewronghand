7d5ad26b9cebfda045e3c2af0c41fb677642ceca
thewronghand <penfreak77@gmail.com>
2023-08-25 23:12:04 +0900

    design: 폰트크기 수정

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="flex justify-center items-center flex-col">
+    <div className="flex justify-center items-center flex-col space-y-4 text-xl">
       <Link to="/about">about</Link>
       <Link to="/projects">projects</Link>
       <Link to="/contacts">contacts</Link>
--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -3,7 +3,7 @@ import Nav from "../Components/Nav";
 export default function Main() {
   return (
     <div className="w-screen h-screen flex flex-col sm:flex-row justify-center items-center">
-      <div className="flex justify-center items-center w-full h-1/5 sm:w-2/5">
+      <div className="flex justify-center items-center w-full h-1/5 sm:w-2/5 text-2xl">
         this is main page
       </div>
       <div className="w-full h-1/5 sm:w-1/5 flex justify-center items-center">

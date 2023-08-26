6347382587e77e2d19c0b699ec4e199124d8ae04
thewronghand <penfreak77@gmail.com>
2023-08-26 09:14:35 +0900

    design: Nav 아이템 호버 시 디자인 수정

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -2,38 +2,43 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="flex justify-center items-center flex-col space-y-4 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+    <div className="flex justify-center flex-col space-y-4 text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
       <div className="flex items-center">
         <div className="mr-2 text-xs sm:text-xs md:text-sm lg:text-base xl:text-lg">
           01
         </div>
         <Link
           to="/about"
-          className="w-24 text-lg sm:text-xl md:text-2xl lg:3xl xl:text-4xl hover:animate-bounce"
+          className=" text-lg sm:text-xl md:text-2xl lg:3xl xl:text-4xl relative group pb-1"
         >
           About
+          <div className="absolute bottom-0 left-0 h-[2px] bg-current w-0 group-hover:w-full transition-all duration-300"></div>
         </Link>
       </div>
+
       <div className="flex items-center">
         <div className="mr-2 text-xs sm:text-xs md:text-sm lg:text-base xl:text-lg">
           02
         </div>
         <Link
           to="/projects"
-          className="w-24 text-lg sm:text-xl md:text-2xl lg:3xl xl:text-4xl hover:animate-bounce"
+          className="text-lg sm:text-xl md:text-2xl lg:3xl xl:text-4xl relative group pb-1"
         >
           Projects
+          <div className="absolute bottom-0 left-0 h-[2px] bg-current w-0 group-hover:w-full transition-all duration-300"></div>
         </Link>
       </div>
+
       <div className="flex items-center">
         <div className="mr-2 text-xs sm:text-xs md:text-sm lg:text-base xl:text-lg">
           03
         </div>
         <Link
           to="/contacts"
-          className="w-24 text-lg sm:text-xl md:text-2xl lg:3xl xl:text-4xl hover:animate-bounce"
+          className="text-lg sm:text-xl md:text-2xl lg:3xl xl:text-4xl relative group pb-1"
         >
           Contacts
+          <div className="absolute bottom-0 left-0 h-[2px] bg-current w-0 group-hover:w-full transition-all duration-300"></div>
         </Link>
       </div>
     </div>

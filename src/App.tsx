92f4eefce01d4cc59e66c8ecc3680247ddebe47f
thewronghand <penfreak77@gmail.com>
2023-08-28 22:04:59 +0900

    design: 필름 그레인 효과 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,16 @@ import Main from "./Pages/Main";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
 import Contacts from "./Pages/Contacts";
+import { useGrain } from "./utils/hooks/useGrain";
 
 function App() {
+  const canvasRef = useGrain();
   return (
     <>
+      <canvas
+        ref={canvasRef}
+        className="absolute top-0 left-0 w-full h-full z-10 opacity-5 pointer-events-none"
+      />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />

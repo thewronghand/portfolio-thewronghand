3419047addef82ff6410b4a276f13127d7670d9d
thewronghand <penfreak77@gmail.com>
2023-08-31 06:55:52 +0900

    feat: 각 페이지별 다크모드 스타일 적용

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
+import { useDarkMode } from "../utils/hooks/useDarkMode";
 
 export default function Projects() {
+  const darkMode = useDarkMode();
+
   return (
-    <>
-      this is projects page
-      <Link to="/">return to main</Link>
-    </>
+    <div className={`${darkMode ? "text-white bg-slate-500" : "text-black"}`}>
+      <section className="w-screen h-screen flex flex-col justify-center items-center">
+        this is projects page
+        <Link
+          to="/"
+          className={`${
+            darkMode ? "text-blue-300" : "text-blue-500"
+          } mt-10 inline-block`}
+        >
+          return to main
+        </Link>
+      </section>
+    </div>
   );
 }

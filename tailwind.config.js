c45c870b04ab3fc0186521ca1f03c283de252761
thewronghand <penfreak77@gmail.com>
2023-09-11 18:36:29 +0900

    chore: 테일윈드 config 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./src/**/*.{html,js,tsx,jsx,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      opacity: {
+        5: "0.05",
+      },
+      backdropBlur: {
+        custom: "3px",
+      },
+    },
   },
   plugins: [],
 };

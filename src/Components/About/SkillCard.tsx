26e6b5bf2923d6818b481a1439b0d56dc0c77451
thewronghand <penfreak77@gmail.com>
2023-09-11 18:33:53 +0900

    feat: SkillCard 스타일 적용

--- src/Components/About/SkillCard.tsx
+++ src/Components/About/SkillCard.tsx
@@ -6,13 +6,13 @@ interface SkillCardProps {
 
 export default function SkillCard({ skill }: SkillCardProps) {
   return (
-    <li className="flex flex-col">
+    <li className=" text-gray-700 bg-white flex flex-col max-w-[320px] min-w-[320px] h-[512px] rounded-xl overflow-hidden shadow-xl">
       <section style={{ background: `${skill.bgColorCode}` }}>
         <img src={skill.imgUrl} alt={skill.title} className="w-full" />
       </section>
-      <section>
-        <section>{skill.title}</section>
-        <ul>
+      <section className="p-2 mt-2 h-1/3">
+        <section className="pl-2 font-bold">{skill.title}</section>
+        <ul className=" list-disc p-3 pl-10 h-32 overflow-y-scroll">
           {skill.description.map((item) => (
             <li>{item}</li>
           ))}

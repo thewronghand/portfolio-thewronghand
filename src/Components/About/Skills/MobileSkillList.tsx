08251b1a7e1da3bd0564fd7b9ebeee476b52b8fd
thewronghand <penfreak77@gmail.com>
2023-09-19 18:26:33 +0900

    design: 모바일 스킬리스트 스타일 수정

--- src/Components/About/Skills/MobileSkillList.tsx
+++ src/Components/About/Skills/MobileSkillList.tsx
@@ -21,34 +21,38 @@ export default function MobileSkillList({
   };
 
   return (
-    <section className=" sm:hidden 2xl:block 5xl:hidden">
-      <ul>
+    <section className="flex flex-row sm:hidden 2xl:inline-flex 5xl:hidden">
+      <ul className="w-1/2">
         {data.map((skillSet: SkillSet) => (
           <li
+            key={skillSet.title}
             onClick={() => {
               handleCategoryClick(skillSet);
             }}
             className={`${
               selectedCategory &&
               selectedCategory.title === skillSet.title &&
-              "text-white bg-blue-300 cursor-default"
-            }`}
+              "text-white bg-blue-300 cursor-default  hover:bg-blue-300 hover:cursor-default"
+            } 
+            cursor-pointer p-[1px] m-[1px]
+            `}
           >
             {skillSet.title}
           </li>
         ))}
       </ul>
-      <ul className="flex">
+      <ul className="w-1/2">
         {selectedCategory?.data.map((skill: Skill) => (
           <li
+            key={skill.title}
             onClick={() => {
               onItemClick(skill);
             }}
             className={`${
               selectedSkill &&
               selectedSkill.title === skill.title &&
-              "text-white bg-blue-300 cursor-default"
-            }`}
+              "text-white bg-blue-300 cursor-default  hover:bg-blue-300 hover:cursor-default"
+            } cursor-pointer p-[1px] m-[1px]`}
           >
             {skill.title}
           </li>

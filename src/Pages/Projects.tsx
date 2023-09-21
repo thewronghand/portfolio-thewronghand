9d18f21b64f2d3be194c23eee49be5056625563e
thewronghand <penfreak77@gmail.com>
2023-09-22 01:33:33 +0900

    feat: 캐싱 로직 페이지 단위로 관리

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -3,10 +3,12 @@ import { useDarkMode } from "../utils/hooks/useDarkMode";
 import useFetchCollection from "../utils/hooks/useFetchCollection";
 import { Project } from "../types";
 import ProjectList from "../Components/Projects/ProjectList";
+import { useCacheProjectThumbnails } from "../utils/hooks/useCacheSkillImages";
 
 export default function Projects() {
   const darkMode = useDarkMode();
   const { data, loading, error } = useFetchCollection<Project>("/projects");
+  useCacheProjectThumbnails(data);
 
   return (
     <div

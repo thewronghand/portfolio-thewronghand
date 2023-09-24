ebaa79f55363cb5d403d34d68a52ca89b18b41fb
thewronghand <penfreak77@gmail.com>
2023-09-25 08:17:38 +0900

    chore: 각 섹션 타이틀 커서 default처리

--- src/Components/About/Contacts/ContactsContainer.tsx
+++ src/Components/About/Contacts/ContactsContainer.tsx
@@ -7,12 +7,12 @@ interface ContactContainerProps {
 
 export default function ContactContainer({ data }: ContactContainerProps) {
   return (
-    <section className="flex flex-col items-center">
-      <section className="text-5xl w-full mb-10 border-b-2 pb-5">
+    <section className="flex flex-col items-center mb-80 pl-2">
+      <section className="text-5xl w-full mb-10 border-b-2 pb-5 cursor-default">
         Contacts
       </section>
-      <section>
-        <ul>
+      <section className="w-full px-6">
+        <ul className="flex flex-wrap justify-between">
           {data.data.map((contact: Contact | ContactMisc) => (
             <ContactsItem key={contact.title} contact={contact} />
           ))}

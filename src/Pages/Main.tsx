980178d41f050ed47494501284f201af30022dec
thewronghand <penfreak77@gmail.com>
2023-09-25 09:39:45 +0900

    design: Main페이지 로고 적용

--- src/Pages/Main.tsx
+++ src/Pages/Main.tsx
@@ -8,14 +8,15 @@ export default function Main() {
     <div
       className={`${
         darkMode ? "text-white bg-slate-500" : "text-gray-700"
-      } w-screen h-screen flex flex-col sm:flex-row justify-center items-center transition-all duration-300 ease-in-out`}
+      } w-screen h-screen flex flex-col justify-center items-center transition-all duration-300 ease-in-out`}
     >
-      <Logo />
-      <div className="flex justify-center items-center w-full h-1/5 sm:w-2/5 text-xl sm:text-2xl md:text-3xl lg:4xl xl:text-4xl text-center">
-        프론트엔드 개발자 이의현
-        <br />
-        그른손thewronghand의 포트폴리오입니다.
-      </div>
+      <Logo
+        width="125"
+        height="175"
+        strokeWidth="270"
+        animated={true}
+        vertical={true}
+      />
     </div>
   );
 }

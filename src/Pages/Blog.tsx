d87e647f09ffefe964d85445fbfdde5b05bf27f6
thewronghand <penfreak77@gmail.com>
2023-09-26 17:46:27 +0900

    design: iframe 영역 간단한 반응형 작업

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -6,13 +6,13 @@ export default function Blog() {
   const darkMode = useDarkMode();
   return (
     <main
-      className={`pt-20 w-screen h-screen flex flex-col justify-center items-center transition-all duration-300 ease-in-out ${
+      className={`min-h-screen pt-20 flex flex-col justify-center items-center transition-all duration-300 ease-in-out ${
         darkMode ? "text-white bg-slate-500" : "text-gray-700"
       }`}
     >
       <BlogIntro />
       <motion.div
-        className={`flex justify-center m-10 w-full h-[650px] ${
+        className={`flex justify-center m-10 w-full h-[1000px] md:h-[650px] ${
           darkMode ? "bg-[#6C7A8F]" : "bg-gray-100"
         }`}
         initial={{ opacity: 0, y: -10 }}

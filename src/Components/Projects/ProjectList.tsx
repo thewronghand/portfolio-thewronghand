d8e8955a6c7d66416d81bb2d567f26f0f689f2ba
thewronghand <penfreak77@gmail.com>
2023-09-26 14:36:51 +0900

    feat: 프로젝트리스트 마크업, 반응형 적용

--- src/Components/Projects/ProjectList.tsx
+++ src/Components/Projects/ProjectList.tsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router-dom";
 import { Project } from "../../types";
 import { useState } from "react";
+import ProjectListItem from "./ProjectListItem";
+import ProjectListImgSection from "./ProjectListImgSection";
+import MobileImgSection from "./MobileImgSection";
 
 interface ProjectListProps {
   data: Project[];
@@ -10,32 +12,27 @@ export default function ProjectList({ data }: ProjectListProps) {
   const [hoveredItem, setHoveredItem] = useState<Project | null>(null);
 
   return (
-    <main>
-      <section className="hidden sm:block fixed bottom-0 left-0">
-        {hoveredItem && (
-          <section style={{ background: `${hoveredItem.thumbnailBgColor}` }}>
-            <img
-              key={hoveredItem.title}
-              src={hoveredItem.thumbnailLogo}
-              alt={hoveredItem.title}
-            />
+    <main className="w-full h-full flex flex-col md:flex-row">
+      <MobileImgSection hoveredItem={hoveredItem} />
+      <ProjectListImgSection hoveredItem={hoveredItem} />
+      <section className="md:w-1/2 md:flex md:items-center lg:mr-10">
+        <section className="w-full 3xl:w-10/12">
+          <section className="mx-6 flex items-center justify-between">
+            <section className=" text-5xl pb-4">Projects</section>
+            <section className="mr-4 text-3xl">{data.length} projects</section>
           </section>
-        )}
+
+          <ul className="flex flex-col w-full overflow-y-auto px-6">
+            {data.map((item: Project) => (
+              <ProjectListItem
+                key={item.title}
+                item={item}
+                onHover={setHoveredItem}
+              />
+            ))}
+          </ul>
+        </section>
       </section>
-      <ul className="flex flex-col">
-        {data.map((item: Project) => (
-          <Link
-            to={`${item.id}`}
-            onMouseOver={() => {
-              setHoveredItem(item);
-              console.log("asdf");
-            }}
-            onMouseOut={() => setHoveredItem(null)}
-          >
-            {item.title}
-          </Link>
-        ))}
-      </ul>
     </main>
   );
 }

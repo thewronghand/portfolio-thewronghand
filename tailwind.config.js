9a7050345bdc930608f12cd5c3eac8b8c151d859
thewronghand <penfreak77@gmail.com>
2023-09-26 14:36:16 +0900

    chore: SkillList용 boxShadow 유틸리티 클래스 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,11 @@ export default {
         "5xl": "2000px",
         "6xl": "2300px",
       },
+      boxShadow: {
+        "y-shadow": "0 4px 4px -1px rgba(0, 0, 0, 0.06)",
+        "first-shadow":
+          "0 4px 4px -1px rgba(0, 0, 0, 0.06), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
+      },
     },
   },
   plugins: [],

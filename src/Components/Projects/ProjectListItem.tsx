45df4fac04d818a25dbe30d2baca8a1e6a0c42bc
thewronghand <penfreak77@gmail.com>
2023-09-26 14:36:38 +0900

    feat: ProjectList에 띄울 리스트아이템 컴포넌트 구현 및 마크업 적용

---
+++ src/Components/Projects/ProjectListItem.tsx
@@ -0,0 +1,65 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { Project } from "../../types";
+import { useState } from "react";
+
+interface ProjectListItemProp {
+  item: Project;
+  onHover: (item: Project | null) => void;
+}
+
+const arrowVariants = {
+  hidden: { opacity: 0, x: -10 },
+  visible: { opacity: 1, x: 0 },
+};
+
+const textVariants = {
+  hidden: { x: 0 },
+  visible: { x: 20 },
+};
+
+export default function ProjectListItem({
+  item,
+  onHover,
+}: ProjectListItemProp) {
+  const [isHovered, setHovered] = useState(false);
+
+  return (
+    <li className="w-full border-t-[1px] last:border-b-[1px] py-4 px-4 lg:py-5 relative">
+      <Link to={item.id}>
+        <section
+          className="flex justify-between"
+          onMouseOver={() => {
+            onHover(item);
+            setHovered(true);
+          }}
+          onMouseOut={() => {
+            onHover(null);
+            setHovered(false);
+          }}
+        >
+          <motion.div
+            initial="hidden"
+            animate={isHovered ? "visible" : "hidden"}
+            variants={textVariants}
+            transition={{
+              type: "spring",
+              stiffness: 700,
+              damping: 30,
+            }}
+            className="flex items-center"
+          >
+            <motion.span
+              variants={arrowVariants}
+              className="absolute left-[-1.5rem] text-xl"
+            >
+              ➔
+            </motion.span>
+            <span className="text-lg lg:text-xl">{item.title}</span>
+          </motion.div>
+          <span className="lg:text-lg">{item.type}</span>
+        </section>
+      </Link>
+    </li>
+  );
+}

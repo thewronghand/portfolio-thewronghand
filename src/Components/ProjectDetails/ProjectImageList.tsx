c812d17310cd98e8ee22ef89fad593d383d6362d
thewronghand <penfreak77@gmail.com>
2023-09-29 01:04:14 +0900

    feat: ProjectImageList 컴포넌트 구현

--- src/Components/ProjectDetails/ProjectImageList.tsx
+++ src/Components/ProjectDetails/ProjectImageList.tsx
@@ -4,12 +4,17 @@ interface ProjectImageListProps {
 
 export default function ProjectImageList({ imgs }: ProjectImageListProps) {
   return (
-    <ul>
-      {imgs.map((item: string, index) => (
-        <li key={index}>
-          <img src={item} alt={String(index)} />
-        </li>
-      ))}
-    </ul>
+    <section className=" mb-32">
+      <section className="text-3xl pt-4 pb-2 border-b-2 border-solid mb-4">
+        Sample Images
+      </section>
+      <ul className="px-6">
+        {imgs.map((item: string, index) => (
+          <li key={index} className="my-4 shadow-md rounded-xl overflow-hidden">
+            <img src={item} alt={String(index)} />
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }

34790fe65c9055e89bf7d7ed0920e8a329cce271
thewronghand <penfreak77@gmail.com>
2023-10-13 05:16:35 +0900

    design: 샘플 컬러스킴 상수 변수 선언

--- src/utils/constant/index.ts
+++ src/utils/constant/index.ts
@@ -5,3 +5,38 @@ export const COLORS = {
   LIGHT_MODE_BG_WHITE: "bg-white",
   LIGHT_MODE_TEXT: "text-gray-700",
 };
+
+export const COLOR_SCHEMES = {
+  TIEFENWALD: {
+    DARK: {
+      BG: "#2c362b",
+      TEXT: "#f5e8cc",
+      PRIMARY: "#f5b19e",
+      SECONDARY: "#a1b49c",
+      ACCENT: "#de3b00",
+    },
+    LIGHT: {
+      BG: "#fef2e2",
+      TEXT: "#191d0f",
+      PRIMARY: "#f5b19e",
+      SECONDARY: "#50634b",
+      ACCENT: "#de3b00",
+    },
+  },
+  HARBOR: {
+    DARK: {
+      BG: "#4F4F5C",
+      TEXT: "#EEF1F2",
+      PRIMARY: "#F2B5B5",
+      SECONDARY: "#6CC1C1",
+      ACCENT: "#EDF6BA",
+    },
+    LIGHT: {
+      BG: "#e6f3f3",
+      TEXT: "#4f4f5c",
+      PRIMARY: "#F2B6B6",
+      SECONDARY: "#3E9393",
+      ACCENT: "#EDF6BA",
+    },
+  },
+};

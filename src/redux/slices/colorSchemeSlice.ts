8add8efd4e9601ccbcaf506f08de9e84083c0f44
thewronghand <penfreak77@gmail.com>
2023-10-13 07:24:31 +0900

    feat: 컬러스킴 새로고침 시 초기화 안되도록 수정

--- src/redux/slices/colorSchemeSlice.ts
+++ src/redux/slices/colorSchemeSlice.ts
@@ -1,11 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { COLOR_SCHEMES } from "../../utils/constant";
 
+const savedColorScheme = sessionStorage.getItem("colorScheme");
+
+const initialState = savedColorScheme
+  ? COLOR_SCHEMES[savedColorScheme as keyof typeof COLOR_SCHEMES]
+  : COLOR_SCHEMES.HARBOR;
+
 export const colorSchemeSlice = createSlice({
   name: "colorScheme",
-  initialState: COLOR_SCHEMES.TIEFENWALD,
+  initialState: initialState,
   reducers: {
-    setColorScheme: (_state, action) => action.payload,
+    setColorScheme: (_state, action) => {
+      const schemeName = Object.keys(COLOR_SCHEMES).find(
+        (key) =>
+          COLOR_SCHEMES[key as keyof typeof COLOR_SCHEMES] === action.payload
+      );
+      if (schemeName) {
+        sessionStorage.setItem("colorScheme", schemeName);
+      }
+      return action.payload;
+    },
   },
 });
 

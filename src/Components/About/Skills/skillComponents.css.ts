190cbb52c01f668b230a0505088a12c2828a01e4
thewronghand <penfreak77@gmail.com>
2023-10-16 18:31:31 +0900

    design:SkillList 스타일 적용

--- src/Components/About/Skills/skillComponents.css.ts
+++ src/Components/About/Skills/skillComponents.css.ts
@@ -1,4 +1,5 @@
 import { style } from "@vanilla-extract/css";
+import { mediaQueries } from "../../../styles/mediaQueries";
 
 export const skillCardStyle = {
   container: style({
@@ -34,5 +35,59 @@ export const skillCardStyle = {
     paddingLeft: "2.5rem",
     height: "8rem",
     listStyleType: "disc",
+  }),
+};
+
+export const skillListStyle = {
+  container: style({
+    display: "none",
+    listStyle: "none",
+    padding: "0.75rem",
+    borderRadius: "0.75rem",
+    boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+    width: "320px",
+    "@media": {
+      [mediaQueries.SCREEN_SM]: { display: "block" },
+      [mediaQueries.SCREEN_MD]: {
+        marginTop: "0",
+      },
+      [mediaQueries.SCREEN_2XL]: { display: "none" },
+      [mediaQueries.SCREEN_5XL]: { display: "block", width: "100%" },
+    },
+  }),
+  skillSetTitle: style({
+    cursor: "default",
+    margin: "0.5rem",
+    fontSize: "1.125rem",
+    lineHeight: "1.75rem",
+  }),
+  list: style({
+    display: "flex",
+    flexDirection: "column",
+    listStyle: "none",
+    paddingLeft: "0",
+  }),
+  item: style({
+    paddingLeft: "0.5rem",
+    paddingRight: "0.5rem",
+    paddingTop: "0.25rem",
+    paddingBottom: "0.25rem",
+    marginLeft: "0.25rem",
+    marginRight: "0.25rem",
+    borderRadius: "0.375rem",
+    transitionProperty: "all",
+    transitionTimingFunction: [
+      "cubic-bezier(0.4, 0, 0.2, 1)",
+      "cubic-bezier(0.4, 0, 0.2, 1)",
+    ],
+    transitionDuration: "300ms",
+    cursor: "pointer",
+    ":hover": { backgroundColor: "#DBEAFE" },
+  }),
+  itemSelected: style({
+    color: "#ffffff",
+    backgroundColor: "#60A5FA",
+    cursor: "default",
+    ":hover": { backgroundColor: "#60A5FA", cursor: "default" },
   }),
 };

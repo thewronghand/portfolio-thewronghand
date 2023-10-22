28bc52967dd440ed6ae194fdc6f236e1f476fedb
thewronghand <penfreak77@gmail.com>
2023-10-23 02:35:11 +0900

    fix: 여백 관련 스타일 수정

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -3,6 +3,7 @@ import BlogIntro from "../Components/Blog/BlogIntro";
 import { useEffect } from "react";
 import { useColorScheme } from "../utils/hooks/useColorScheme";
 import { blogStyle as style } from "./pages.css";
+import Background from "../Components/Global/Background";
 
 export default function Blog() {
   const colorScheme = useColorScheme();
@@ -11,20 +12,22 @@ export default function Blog() {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <main className={style.container}>
-      <BlogIntro />
-      <motion.div
-        style={{ background: colorScheme.LIGHT.BG }}
-        className={style.iframeSection}
-        initial={{ opacity: 0, y: -10 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.6, duration: 0.5 }}
-      >
-        <iframe
-          src="https://vitriol-345e3.web.app/"
-          className={style.iframe}
-        ></iframe>
-      </motion.div>
-    </main>
+    <Background>
+      <main className={style.container}>
+        <BlogIntro />
+        <motion.div
+          style={{ background: colorScheme.LIGHT.BG }}
+          className={style.iframeSection}
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.6, duration: 0.5 }}
+        >
+          <iframe
+            src="https://vitriol-345e3.web.app/"
+            className={style.iframe}
+          ></iframe>
+        </motion.div>
+      </main>
+    </Background>
   );
 }
